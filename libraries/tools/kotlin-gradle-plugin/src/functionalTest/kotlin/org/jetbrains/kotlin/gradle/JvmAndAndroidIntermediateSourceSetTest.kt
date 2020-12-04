a967a385e45682788859eb7384b90a9adb75333c
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2020-12-04 17:04:39 +0300

    Revert "Add comment about unsupported associate compilations to functionalTest source files"
    This reverts commit 7c7eada4

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/JvmAndAndroidIntermediateSourceSetTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/JvmAndAndroidIntermediateSourceSetTest.kt
@@ -3,7 +3,6 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-/* Associate compilations are not yet supported by the IDE. KT-34102 */
 @file:Suppress("invisible_reference", "invisible_member", "FunctionName", "DuplicatedCode")
 
 package org.jetbrains.kotlin.gradle
--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/KotlinAndroidDependsOnEdgesTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/KotlinAndroidDependsOnEdgesTest.kt
@@ -2,8 +2,7 @@
  * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
-
-/* Associate compilations are not yet supported by the IDE. KT-34102 */
+// TODO KT-34102
 @file:Suppress("invisible_reference", "invisible_member", "FunctionName")
 package org.jetbrains.kotlin.gradle
 
